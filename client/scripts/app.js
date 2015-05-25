7c87a6a871d06b56b6e468ada9d160ff1f3c3198
tchan247 <tchan247@gmail.com>
2015-05-25 15:25:22 -0700

    up to remove messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,44 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.init = function(){};
+
+app.send = function(message){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent: ', data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
+  $.ajax({
+    // always use this url
+    url: undefined,
+    type: 'GET',
+    //data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      return data;
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message');
+    }
+  });
+};
+
+app.clearMessages = function(){
+  $('#chats').remove();
+};
+
